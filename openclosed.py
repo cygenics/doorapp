78bed1f52ecfc7b6be2bbb422ba355901c7b7283
cygenics <cygenics@gmail.com>
2015-07-10 12:03:31 -0500

    af

--- openclosed.py
+++ openclosed.py
@@ -34,7 +34,7 @@ def change_dooralert_state(state):
 		name = "Stan"
 		number = os.getenv(name)
 		send_text(number, name)
-		time.sleep(10)
+		time.sleep(5)
 		make_call(number, name)
 	else:
 		#door is closed
