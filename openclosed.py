0ef4bea189d1237f9a2153becaae6105ea0fe11c
cygenics <cygenics@gmail.com>
2015-07-13 12:31:33 -0500

    a

--- openclosed.py
+++ openclosed.py
@@ -7,21 +7,25 @@ from config import fbRef, twilio_acc_id, twilio_acc_auth_token, twilio_number, a
 
 FIREBASE = firebase.FirebaseApplication(fbRef, None)
 
-def change_dooralert_state(state):
-	if state == 0:
-		FIREBASE.put('/', 'dooralert', 'false')
-	else:
-		FIREBASE.put('/', 'dooralert', 'true')
-		
+def send_text(text):
 	client = TwilioRestClient(twilio_acc_id, twilio_acc_auth_token)
 	message = client.messages.create(body=alert_user_name + " the door has been opened!",
 		to=alert_user,
-	    from_="+19723626631")
+		from_="+19723626631")
 	print "text sent"
-	time.sleep (3)
 
+def make_call(call):
 	client = TwilioRestClient(twilio_acc_id, twilio_acc_auth_token)
 	call = client.calls.create(url="https://dooralert.firebaseapp.com/voice.xml",
 		to=alert_user,
 		from_="+19723626631")
 	print "call made"
+
+def change_dooralert_state(state):
+	if state == 0:
+		FIREBASE.put('/', 'dooralert', 'false')
+		send_text(text)
+		time.sleep(4)
+		make_call(call)
+	else:
+		FIREBASE.put('/', 'dooralert', 'true')
